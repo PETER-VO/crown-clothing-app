4c91be1d103382a18d990d3b085c2e7b554e1ebd
Phong <v.ducphong@gmail.com>
2021-06-26 22:17:14 +0300

    update our menu-items to navigate withRouter HOC

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,9 +1,21 @@
+import { Route, Switch } from 'react-router';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
-    <HomePage />
+    <div>
+      <Switch>
+          <Route exact path='/' component={HomePage}/>
+          <Route path='/shop/hats' component={HatsPage}/>
+      </Switch>
+    </div>
   );
 }
 
