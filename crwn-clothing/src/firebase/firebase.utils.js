af78a55682f5b4c516f0114d7ebf5476d2a5924f
Phong <v.ducphong@gmail.com>
2021-06-28 20:41:49 +0300

    integrated firebase into application, created sign-in page with sign-in component and sign-in with google

---
+++ crwn-clothing/src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyBazjLGMUVcFpMKRg6wMFXTtI4e2pZJok4",
+    authDomain: "crwndb-8f8cd.firebaseapp.com",
+    projectId: "crwndb-8f8cd",
+    storageBucket: "crwndb-8f8cd.appspot.com",
+    messagingSenderId: "975861692563",
+    appId: "1:975861692563:web:12d76137990396fb63b566",
+    measurementId: "G-DH4JZV7LF2"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account'});
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
---
+++ crwn-clothing/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import SignIn from '../../component/sign-in/sign-in.component';
+
+const SignInAndSignUp = () => (
+    <div>
+       <SignIn />
+    </div>
+)
+
+export default SignInAndSignUp;
