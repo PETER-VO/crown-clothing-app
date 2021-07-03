c12fdad4de19284ce921308cc7dcec269b66f51c
Phong <v.ducphong@gmail.com>
2021-07-03 22:51:51 +0300

    implemented firebase utils, included ability to store authenticated users into our firestore database

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router';
 import './App.css';
 import Header from './component/header/header.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shop.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
@@ -20,9 +20,19 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => { 
-      this.setState({currentUser: user});
-      console.log(user); 
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+      
+        userRef.onSnapshot(snapShot => {
+          this.setState({currentUser: {
+            id: snapShot.id,
+            ...snapShot.data()
+          }});
+        })
+      }
+      
+     
     });
   }
 
--- crwn-clothing/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ crwn-clothing/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import SignIn from '../../component/sign-in/sign-in.component';
+import SignUp from '../../component/sign-up/sign-up.component';
+
+import './sign-in-and-sign-up.styles.scss';
 
 const SignInAndSignUp = () => (
-    <div>
+    <div className='sign-in-and-sign-up'>
        <SignIn />
+       <SignUp />
     </div>
 )
 
