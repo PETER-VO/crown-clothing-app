d45aac4bf8a693c16b2af27571df88fc8b1089cc
Phong <v.ducphong@gmail.com>
2021-07-07 13:02:40 +0300

    added checkout-page, checkout-item component

--- crwn-clothing/src/component/cart-dropdown/cart-dropdown.component.jsx
+++ crwn-clothing/src/component/cart-dropdown/cart-dropdown.component.jsx
@@ -6,8 +6,9 @@ import CartItem from '../cart-item/cart-item.component';
 import CustomButton from '../custom-button/custom-button.component';
 import { withRouter } from 'react-router-dom';
 import './cart-dropdown.styles.scss';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
-const CartDropDown = ({cartItems, history}) => (
+const CartDropDown = ({cartItems, history, dispatch}) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
             {
@@ -17,7 +18,10 @@ const CartDropDown = ({cartItems, history}) => (
                 <span className='empty-message'>Your cart is empty</span>
             }
         </div>
-        <CustomButton onClick={() => history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
+        <CustomButton onClick={() => {
+            history.push('/checkout');
+            dispatch(toggleCartHidden())
+        }}>GO TO CHECKOUT</CustomButton>
     </div>
 );
 
